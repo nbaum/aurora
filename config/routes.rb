59cb46d2e56f9d5f2a01732d6508147b98d29a25
Nathan Baum <n@p12a.org.uk>
2014-09-13 00:33:58 +0100

    Template-checking routing constraint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
+class IfViewExists < ActionView::LookupContext
+  def initialize
+    super(p Rails.root.join('app', 'views'))
+  end
+  def matches? (request)
+    template_exists?("pages" + File.expand_path(request.params["path"], "/"))
+  end
+end
+
 Rails.application.routes.draw do
 end
