f9bd22a88fc7045adfcf2d41aae646d03c2f6ab8
Nathan Baum <n@p12a.org.uk>
2014-09-22 19:17:06 +0100

    Rails scaffold template

---
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -0,0 +1,57 @@
+<% if namespaced? -%>
+require_dependency "<%= namespaced_file_path %>/application_controller"
+
+<% end -%>
+<% module_namespacing do -%>
+class <%= controller_class_name %>Controller < ApplicationController
+
+  before_action :set_<%= plural_table_name %>
+  before_action :set_<%= singular_table_name %>, except: [:new]
+
+  def new
+    @<%= singular_table_name %> = <%= orm_class.build("@#{plural_table_name}") %>
+  end
+
+  def create
+    @<%= singular_table_name %> = <%= orm_class.build("@#{plural_table_name}", "#{singular_table_name}_params") %>
+    if @<%= orm_instance.save %>
+      redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully created.'" %>
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @<%= orm_instance.update("#{singular_table_name}_params") %>
+      redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully updated.'" %>
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @<%= orm_instance.destroy %>
+    redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} was successfully destroyed.'" %>
+  end
+
+  private
+
+  def set_<%= plural_table_name %>
+    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+  end
+
+  def set_<%= singular_table_name %>
+    return if params[:id].nil?
+    @<%= singular_table_name %> = <%= orm_class.find("@#{plural_table_name}", "params[:id]") %>
+  end
+
+  def <%= singular_table_name%>_params
+    <%- if attributes_names.empty? -%>
+    params[:<%= singular_table_name %>]
+    <%- else -%>
+    params.require(:<%= singular_table_name %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
+    <%- end -%>
+  end
+
+end
+<% end -%>
