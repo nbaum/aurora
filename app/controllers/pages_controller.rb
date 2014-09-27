998768a80ecea9e908b95ca62dee9ccd5140b98a
Nathan Baum <n@p12a.org.uk>
2014-09-27 22:28:47 +0100

    Index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,2 @@
 class PagesController < ApplicationController
-
-  def view
-    path = File.expand_path(params[:path] || "index", "/")[1..-1]
-    @main_class = params[:path].split("/").join(" ") rescue nil
-    render "pages/#{path}"
-  end
-
 end
