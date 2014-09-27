f47fd1f27c54b440015f20546c9df9df05b0b2f0
Nathan Baum <n@p12a.org.uk>
2014-09-27 01:40:06 +0100

    Some changes

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'jquery-ui-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 
+gem 'bcrypt'
+
 group :doc do
   gem 'sdoc', '~> 0.4.0'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,2 @@
-class IfViewExists < ActionView::LookupContext
-  def initialize
-    super(Rails.root.join('app', 'views'))
-  end
-  def matches? (request)
-    template_exists?("pages" + File.expand_path(request.params["path"], "/"))
-  end
-end
-
 Rails.application.routes.draw do
-  root 'pages#view'
-  get  '*path', to: 'pages#view', constraints: IfViewExists.new
 end
