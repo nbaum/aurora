5f83a52ac21c0908f4fa6bfef2b77647286fa8f1
Nathan Baum <n@p12a.org.uk>
2015-01-19 02:00:50 +0000

    Fix volumes not being put in pool when imported to pool...

--- app/models/storage_pool.rb
+++ app/models/storage_pool.rb
@@ -6,7 +6,7 @@ class StoragePool < ActiveRecord::Base
 
   def refresh
     host.api.list_volumes(pool: self.path).each do |data|
-      v = volumes.where(path: data[:name]).first_or_initialize(size: data[:size], ephemeral: false)
+      v = volumes.where(path: data[:name]).first_or_initialize(size: data[:size], ephemeral: false, pool: self)
       v.optical = !!(data[:name] =~ /\.iso$/)
       v.name ||= v.path
       v.ephemeral = !v.ephemeral
