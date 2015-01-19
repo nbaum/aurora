b2d0048e686574ba2503824dab87329d8742d96a
Nathan Baum <n@p12a.org.uk>
2015-01-19 02:01:23 +0000

    Actually, don't allow volumes to lie about what pool they're in.

--- app/models/storage_pool.rb
+++ app/models/storage_pool.rb
@@ -6,10 +6,11 @@ class StoragePool < ActiveRecord::Base
 
   def refresh
     host.api.list_volumes(pool: self.path).each do |data|
-      v = volumes.where(path: data[:name]).first_or_initialize(size: data[:size], ephemeral: false, pool: self)
+      v = volumes.where(path: data[:name]).first_or_initialize(size: data[:size], ephemeral: false)
       v.optical = !!(data[:name] =~ /\.iso$/)
       v.name ||= v.path
       v.ephemeral = !v.ephemeral
+      v.pool = self
       v.save!
     end
   end
