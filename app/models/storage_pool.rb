0ddc5dbab7437f22ec5b1d829ae1599e779de461
Nathan Baum <n@p12a.org.uk>
2015-01-19 02:45:11 +0000

    Reset the size.

--- app/models/storage_pool.rb
+++ app/models/storage_pool.rb
@@ -6,11 +6,12 @@ class StoragePool < ActiveRecord::Base
 
   def refresh
     host.api.list_volumes(pool: self.path).each do |data|
-      v = volumes.where(path: data[:name]).first_or_initialize(size: data[:size], ephemeral: false)
+      v = volumes.where(path: data[:name]).first_or_initialize(ephemeral: false)
       v.optical = !!(data[:name] =~ /\.iso$/)
       v.name ||= v.path
       v.ephemeral = !v.ephemeral
       v.pool = self
+      v.size = data[:size]
       v.save!
     end
   end
