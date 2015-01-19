4ff388300ef7e080c7a4b29d260c648b2a4c881f
Nathan Baum <n@p12a.org.uk>
2015-01-19 01:42:08 +0000

    Don't clear the ephemeral bit on existing volumes.

--- app/models/storage_pool.rb
+++ app/models/storage_pool.rb
@@ -6,9 +6,8 @@ class StoragePool < ActiveRecord::Base
 
   def refresh
     host.api.list_volumes(pool: self.path).each do |data|
-      v = volumes.where(path: data[:name]).first_or_initialize(size: data[:size])
+      v = volumes.where(path: data[:name]).first_or_initialize(size: data[:size], ephemeral: false)
       v.optical = !!(data[:name] =~ /\.iso$/)
-      v.ephemeral = false
       v.name ||= v.path
       v.save!
     end
