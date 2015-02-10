05e277791e5b0fe15bd95534d055e3c7c93ca7b0
Nathan Baum <n@p12a.org.uk>
2015-02-10 17:13:52 +0000

    New frontend.

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,4 @@
-role :app, %w{aurora@46.30.8.50}
-role :web, %w{aurora@46.30.8.50}
-role :db,  %w{aurora@46.30.8.50}
+role :app, %w{aurora@46.30.8.59}
+role :web, %w{aurora@46.30.8.59}
+role :db,  %w{aurora@46.30.8.59}
 set :rails_env, 'production'
