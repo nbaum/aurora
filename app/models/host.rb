0ac4d9c4ec2c46d21971c53037b802f351e33e9a
Nathan Baum <n@p12a.org.uk>
2015-02-11 13:37:47 +0000

    Helpers to select hosts which have storage or compute resources.

--- app/models/host.rb
+++ app/models/host.rb
@@ -4,6 +4,9 @@ class Host < ActiveRecord::Base
   has_many :servers
   has_many :pools, class_name: 'StoragePool'
 
+  scope :storage, -> { where(has_storage: true) }
+  scope :compute, -> { where(has_compute: true) }
+
   def api (args = {})
     zone.api(url, args)
   end
