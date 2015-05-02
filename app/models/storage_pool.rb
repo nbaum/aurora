38172986568ae24c43717665b63fb78420024581
Nathan Baum <n@p12a.org.uk>
2015-05-02 21:18:35 +0100

    Import new pool volumes correctly.

--- app/models/storage_pool.rb
+++ app/models/storage_pool.rb
@@ -1,14 +1,13 @@
 class StoragePool < ActiveRecord::Base
   belongs_to :account
   belongs_to :host
-  
+
   has_many :volumes
 
   def refresh
     host.api.list_volumes(pool: self.path).each do |data|
-      v = volumes.where(path: data[:name]).first_or_initialize(ephemeral: false)
+      v = volumes.where(path: data[:name]).first_or_initialize(path: data[:name], name: data[:name], ephemeral: false)
       v.optical = !!(data[:name] =~ /\.iso$/)
-      v.name ||= v.path
       v.ephemeral = !v.ephemeral
       v.pool = self
       v.size = data[:size]
