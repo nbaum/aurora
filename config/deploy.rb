411fc12f0c4906ef5357323dca53d6df29ae82a2
Nathan Baum <n@p12a.org.uk>
2015-05-02 21:18:55 +0100

    New deployment settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,22 @@
 set :application, 'aurora'
 set :repo_url, 'aurora@84.45.122.187:repo'
+set :use_sudo, nil
 
 set :deploy_to, '/home/aurora'
 
-set :linked_dirs, %w{var}
+set :linked_dirs, %w{var tmp}
 set :linked_files, %w{.env}
 
 set :keep_releases, 5
 
 namespace :deploy do
+
+  task :restart do
+    on roles :root do
+      execute :cp, "~aurora/current/aurora.service", "/etc/systemd/system"
+      execute :systemctl, "daemon-reload"
+      execute :systemctl, "restart aurora"
+    end
+  end
+
 end
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,18 @@
-role :app, %w{aurora@46.30.8.59}
-role :web, %w{aurora@46.30.8.59}
-role :db,  %w{aurora@46.30.8.59}
+set :rvm_roles, [:app]
 set :rails_env, 'production'
+
+role :root, %w(
+	root@46.30.8.59
+), no_release: true
+
+role :app, %w(
+	aurora@46.30.8.59
+)
+
+role :web, %w(
+	aurora@46.30.8.59
+)
+
+role :db, %w(
+	aurora@46.30.8.59
+)
