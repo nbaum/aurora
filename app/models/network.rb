fb12b15ee17ed51369ebbae8264897f7f29e4703
Nathan Baum <n@p12a.org.uk>
2016-11-29 18:46:40 +0000

    Not any more

--- app/models/network.rb
+++ app/models/network.rb
@@ -9,23 +9,6 @@ class Network < ActiveRecord::Base
   has_many :addresses, dependent: :destroy
   has_many :subnets, dependent: :destroy
 
-  before_save do
-    if first && last
-      (first..last).each do |addr|
-        addresses.where(ip: addr).first_or_create!
-      end
-    end
-  end
-
-  after_save do
-    if subnets.empty?
-      s = subnets.create! kind: "ipv4", prefix: prefix, first: first.to_s, last: last.to_s
-      addresses.each do |addr|
-        addr.update_attributes! subnet: s, network: nil
-      end
-    end
-  end
-
   def allocate_address (kind, to)
     subnets.where(kind: kind).each do |subnet|
       if a = subnet.allocate_address(to)
