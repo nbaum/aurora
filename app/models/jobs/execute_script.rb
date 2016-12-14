f599b0308654cc500145c77c5c00d43664804d6c
Nathan Baum <n@p12a.org.uk>
2016-12-14 12:33:21 +0000

    Multipart scripts with progressive output

--- app/models/jobs/execute_script.rb
+++ app/models/jobs/execute_script.rb
@@ -4,9 +4,15 @@ module Jobs
   class ExecuteScript < Job
 
     def run
+      state["result"] = ""
       script = Script.find(args["script"])
-      path = "/tmp/aurora.script.#{script.id}.sh"
-      server.guest_execute! "cat > #{path} && source #{path} 2>&1 && rm #{path}", script.script
+      script.split(/^##$/).each do |part|
+        part = "set -e\n" + part
+        path = "/tmp/aurora.script.#{script.id}.sh"
+        output = server.guest_execute! "cat > #{path} && source #{path} 2>&1 && rm #{path}", script.script
+        state["result"] << output
+      end
+      state["result"]
     end
 
   end
