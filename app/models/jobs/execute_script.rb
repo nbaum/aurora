bff77b2470596badfcd369dbeeb6ed25f683d48f
Nathan Baum <n@p12a.org.uk>
2016-12-14 12:37:22 +0000

    No idea what that's about

--- app/models/jobs/execute_script.rb
+++ app/models/jobs/execute_script.rb
@@ -6,7 +6,7 @@ module Jobs
     def run
       state["result"] = ""
       script = Script.find(args["script"])
-      script.split(/^##$/).each do |part|
+      script.split(/^\#\#$/).each do |part|
         part = "set -e\n" + part
         path = "/tmp/aurora.script.#{script.id}.sh"
         output = server.guest_execute! "cat > #{path} && source #{path} 2>&1 && rm #{path}", script.script
